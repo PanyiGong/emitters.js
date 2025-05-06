3d5ba0501dd6d65e51f3ae02ecb06815d58e55b9
panyiGong.github.io <panyi_gong@avenues-ny.org>
2025-05-06 09:36:19 -0400

    Update random color

--- particle.js
+++ particle.js
@@ -1,42 +1,37 @@
 class Particle {
-  constructor(x,y) {
-    this.position = createVector(x,y)
-    this.velocity = p5.Vector.random2D()
-    this.acceleration = p5.Vector.random2D()
-    this.r = 10
-    this.lifespan = 255
-  }
-  
-  update() {
-    this.velocity.add(this.acceleration)
-    this.position.add(this.velocity)   
-    this.acceleration.mult(0)
-    this.lifespan -= 2
-  }
-  
-  draw() {
-    fill(255, this.lifespan)
-    circle(this.position.x,this.position.y,this.r)
-  }
-  
-  // f is a p5.Vector
-  applyForce(f) {
-    this.acceleration.add(f)
+  constructor(x, y) {
+    this.position = createVector(x, y);
+    this.velocity = p5.Vector.random2D();
+    this.acceleration = p5.Vector.random2D();
+    this.r = 10;
+    this.lifespan = 255;
+
+    // Assign a random color with alpha
+    this.color = color(random(255), random(255), random(255), this.lifespan);
   }
 
-  
-  isDead() {
-    return this.lifespan < 0
-    // if( this.lifespan < 0 ) {
-    //   return true 
-    // }
-    // return false
+  update() {
+    this.velocity.add(this.acceleration);
+    this.position.add(this.velocity);
+    this.acceleration.mult(0);
+    this.lifespan -= 2;
   }
-  
-  
-  static createStandardParticleAt(x,y) {
-    return new Particle(
-      x,y
-    )
+
+  isDead() {
+    return this.lifespan < 0;
+  }
+
+  draw() {
+    this.color.setAlpha(this.lifespan);  // Update alpha to match lifespan
+    fill(this.color);
+    circle(this.position.x, this.position.y, this.r);
+  }
+
+  applyForce(f) {
+    this.acceleration.add(f);
+  }
+
+  static createStandardParticleAt(x, y) {
+    return new Particle(x, y);
   }
 }
